87948b367f3ade5d548eb5e202d80395ad34a870
Константин Симонов <84959925+KosTyanka@users.noreply.github.com>
2025-02-25 06:03:41 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-// Import Routes & Middleware
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const recipeRoutes = require('./routes/recipeRoutes');
@@ -11,22 +10,18 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(express.static('public'));
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/recipes', recipeRoutes);
 
-// Global Error Handler
 app.use(errorHandler);
 
-// Connect to MongoDB and start the server
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
+  .catch((err) => console.error('MongoDB connection error:', err))
   .then(() => {
     console.log('MongoDB connected.');
     app.listen(process.env.PORT || 3000, () => {
